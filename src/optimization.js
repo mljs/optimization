bfbad75481c419175b7cc15f1c8df7d03d0f6ac2
JeffersonH44 <swrt_9950@hotmail.com>
2015-03-22 23:43:33 -0500

    added Gradient Descent method.

--- src/optimization.js
+++ src/optimization.js
@@ -22,12 +22,15 @@ Optimization = {
     /*
     * Function that obtains the local minimum vector of a function with a vertices of a Simplex.
     *
+    * * Note: function based on the book Numerical Methods by John H. Mathews and Kurtis D. Fink,
+     *       Spanish, Chap 8, pag 450.
+    *
     * @param {Function} f - Function to get the local minimum, this function require one argument and should be a vector.
     * @param {Vector} vertices - Vertices of the Simplex shape.
     * @param {Number} minimumIterations - Minimum number of iterations we want.
     * @param {Number} maximumIterations - Maximum number of iterations we want.
     * @param {Number|BigNumber} epsilon - Local minimum tolerance.
-    * @return {Object} result - return an object that contains a minimum vector of the function and the evaluation of the function in this vector.
+    * @return {Object} result - return an object that contains a minimum vector (.vector) of the function and the evaluation of the function in this vector (.eval).
     *
     * */
     nelderMead : function (f, vertices, minimumIterations, maximumIterations, epsilon) {
@@ -136,5 +139,127 @@ Optimization = {
         var V0 = math.subset(vertices, math.index(low, [0, size]));
         var y0 = f(this._toNormalVector(V0));
         return {vector: this._toNormalVector(V0), eval: y0};
+    },
+
+    /*
+    * Function that get the local minimum of a function "f" with the gradient descent method
+    *
+    * Note: function based on the book Numerical Methods by John H. Mathews and Kurtis D. Fink,
+    *       Spanish, Chap 8, pag 456.
+    *
+    * @param {Function} f - Function to get the local minimum, this function require one argument and should be a vector.
+    * @param {Function} g - Function that represent the gradient of the function f
+    * @param {Vector} p0 - Initial point approximation
+    * @param {Number} maxIterations - Max iterations that we want
+    * @param {Number|BigNumber} delta - Tolerance in process of search direction
+    * @param {Number|BigNumber} epsilon - Tolerance for error y
+    * @return {Object} result - return an object that contains a minimum vector (.vector) of the function and the evaluation of the function in this vector (.eval).
+    *
+    * */
+
+    gradientDescent : function (f, g, p0, maxIterations, delta, epsilon) {
+        var n = math.subset(math.size(p0), math.index(0));
+        var maxj = 10;
+        var big = 1e8;
+        var h = 1;
+        var P = math.zeros(maxj, n+1); // ojo con el tamaño
+        var len = math.norm(p0);
+        var y0 = f(p0);
+
+        if(len > 1e4) h = math.divide(len, 1e4);
+
+        var err = 1, counter = 0, condition = 0;
+        math.subset(P, math.index(counter + 1, [0, n]), p0);
+        math.subset(P, math.index(counter + 1, n), y0);
+
+        while(counter < maxIterations && condition != 5 && (math.larger(h, delta) || math.larger(err, epsilon))) {
+            var S = g(p0);
+
+            var p1 = math.add(p0, math.multiply(h, S));
+            var p2 = math.add(p0, math.multiply(math.multiply(h, 2), S));
+            var y1 = f(p1);
+            var y2 = f(p2);
+
+            condition = 0;
+            var j = 0;
+            while(j < maxj && condition == 0) {
+                len = math.norm(p0);
+
+                if (math.smaller(y0, y1)) {
+                    p2 = p1;
+                    y2 = y1;
+                    h = math.divide(h, 2);
+                    p1 = math.add(p0, math.multiply(h, S));
+                    y1 = f(p1);
+                } else {
+                    if(math.smaller(y2, y1)) {
+                        p1 = p2;
+                        y1 = y2;
+                        h = math.multiply(2, h);
+                        p2 = math.add(p0, math.multiply(math.multiply(2, h), S));
+                        y2 = f(p2);
+                    } else {
+                        condition = -1;
+                    }
+                }
+                j += 1;
+                if (math.smaller(h, delta)) condition = 1;
+                if (math.larger(math.abs(h), big) || math.larger(len, big)) condition = 5;
+            }
+
+            var pmin, ymin, hmin;
+
+            if(condition == 5) {
+                pmin = p1;
+                ymin = y1;
+            } else {
+                var d = math.subtract(math.multiply(4, y1), math.subtract(math.multiply(2, y0), math.multiply(2, y2)));
+                if(math.smaller(d, 0)) {
+                    var mult = math.subtract(math.multiply(4, y1), math.subtract(math.multiply(3, y0), y2));
+                    hmin = math.divide(math.multiply(h, mult), d);
+                } else {
+                    condition = 4;
+                    hmin = math.divide(h, 3);
+                }
+
+                pmin = math.add(p0, math.multiply(hmin, S));
+                ymin = f(pmin);
+
+                var h0 = math.abs(hmin);
+                var h1 = math.abs(math.subtract(hmin, h));
+                var h2 = math.abs(math.subtract(hmin, math.multiply(2, h)));
+
+                if(math.smaller(h0, h)) h = h0;
+                if(math.smaller(h1, h)) h = h1;
+                if(math.smaller(h2, h)) h = h2;
+                if(math.equal(h, 0)) h = hmin;
+                if(math.smaller(h, delta)) condition = 1;
+
+                var e0 = math.abs(math.subtract(y0, ymin));
+                var e1 = math.abs(math.subtract(y1, ymin));
+                var e2 = math.abs(math.subtract(y2, ymin));
+
+                if(math.unequal(e0, 0) && math.smaller(e0, err)) err = e0;
+                if(math.unequal(e1, 0) && math.smaller(e1, err)) err = e1;
+                if(math.unequal(e2, 0) && math.smaller(e2, err)) err = e2;
+                if(math.equal(e0, 0) && math.equal(e1, 0) && math.equal(e2, 0)) err = 0;
+
+                if(math.smaller(err, epsilon)) condition = 2;
+                if(condition == 2 && math.smaller(h, delta)) condition = 3;
+            }
+
+            counter++;
+            math.subset(P, math.index(counter + 1, [0, n]), pmin);
+            math.subset(P, math.index(counter + 1, n), ymin);
+
+            p0 = pmin;
+            y0 = ymin;
+
+            var ptemp = [];
+            for (var i = 0; i < p0._data.length; ++i) ptemp.push(p0._data[i]);
+            p0 = ptemp;
+        }
+
+        return {vector: p0, eval: y0};
     }
 };
--- test/Test.js
+++ test/Test.js
@@ -19,4 +19,29 @@ describe('Optimization', function () {
 
         (result.eval).should.be.approximately(-6.99999998, 10e-5);
     });
+
+    it('Gradiend descent', function () {
+        var p0 = [3.5, 2.5];
+        var f = math.eval("f(x) = (subset(x, index(1)) * subset(x, index(1))) - 4 * subset(x, index(1)) + " +
+        "(subset(x, index(2)) * subset(x, index(2))) - subset(x, index(2)) - subset(x, index(1)) * subset(x, index(2))");
+        var g = function(x) {
+            var z = math.zeros(1, 2);
+            var g = math.eval("f(x) = matrix([subtract(multiply(2, subset(x, index(1))), subtract(4, subset(x, index(2))))"  +
+                                            ", subtract(multiply(2, subset(x, index(2))), subtract(1, subset(x, index(1))))])");
+            var valG = g(x);
+            var result = math.multiply(math.divide(1, math.norm(valG)), valG);
+            return math.multiply(-1, result);
+        };
+
+        var result = Optimization.gradientDescent(f, g, p0, 10000, 10e-5, 10e-5);
+
+        var optimumVector = [3.1126511, 1.99983839];
+
+        for(var i = 0; i < optimumVector.length; ++i) {
+            (math.subset(result.vector, math.index(i))).should.be.approximately(optimumVector[i], 10e-2);
+        }
+
+        (result.eval).should.be.approximately(-6.99999998, 10e-2);
+
+    });
 });
