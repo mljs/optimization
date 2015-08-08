c5afe828affd8ffe1a1bab6bf2ff577542b1dae4
andcastillo <andcastillo@gmail.com>
2015-08-07 22:04:27 -0500

    Merge
    Merge

--- src/index.js
+++ src/index.js
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
-/**
- * Created by acastillo on 8/6/15.
- */
-'use strict';
-
-exports.LM = require('./LM');
-=======
 'use strict';
 
 exports.Optimization = require("./optimization");
 exports.DifferentialEquations = require("./differentialEquations");
->>>>>>> origin/master
+exports.LM = require('./LM');
